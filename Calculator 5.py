e33bd83265809246e9b0f5b7a83e57b36fb8336e
Preston <chez.it.kid.2000@gmail.com>
2025-10-30 19:50:38 -0400

    Bug fixes

--- Calculator 5.py
+++ Calculator 5.py
@@ -2,7 +2,7 @@ import requests
 import webbrowser
 
 #Auto update
-CURRENT_VERSION = "V0.3.2"  #Version number
+CURRENT_VERSION = "V0.3.3"  #Version number
 
 #GITHUB RELEASES URL
 GITHUB_API_RELEASES = "https://api.github.com/repos/Calculator-master-Tacomaster31/Calculator-5/releases/latest"
